db8199de073cee4deb102d62041025a87fb3cddc
merona <dksektha0215@naver.ocom>
2015-07-30 16:11:35 +0900

    Create FirstFactorial.rb

---
+++ FirstFactorial.rb
@@ -0,0 +1,17 @@
+def FirstFactorial(num)
+
+  # code goes here
+  if num<= 1
+    1
+  else
+    return num * FirstFactorial(num-1)
+  end
+  
+         
+end
+
+
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+FirstFactorial(STDIN.gets)  
